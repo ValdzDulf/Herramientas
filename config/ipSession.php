52ec0c180e142c0d87b7b9af0fe1e196cf65859c
Diego Valentin <mis.process.cdmx@sertec.com.mx>
2023-03-13 17:31:05 -0600

    core: Adds IP addresses for root user

---
+++ config/ipSession.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ |--------------------------------------------------------------------------
+ | Stores the IP addresses of users for root user login
+ |--------------------------------------------------------------------------
+ |
+ | Defining the filed options.
+ |
+ | [] array Defines the different IP addresses of users.
+ |   [<IP>] array Computer IP address.
+ |     [name]         string Employee's name.
+ |     [employeeCode] string Employee identification code.
+ |
+ */
+return [
+        '<ip>' => [
+                'name' => '<name>',
+                'employeeCode' => '<code>'
+        ]
+];
