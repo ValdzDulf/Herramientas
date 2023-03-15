25c4327c023c19d62cf38d39e4758d15c7ea4615
Diego Valentin <mis.process.cdmx@sertec.com.mx>
2023-03-15 12:41:33 -0600

    core: Adds footer and 401 views

---
+++ core/views/_footer.php
@@ -0,0 +1,97 @@
+        <footer class="mt-5 bg-sertec-secondary">
+            <div class="p-3 container-fluid">
+                <div class="row row-cols-2 row-cols-lg-4">
+                    <div class="col mb-3 mb-xl-0">
+                        <div class="card-body">
+                            <h6 class="text-light">Karen I. Martínez Vargas</h6>
+                            <ul class="mb-0 list-unstyled text-white-50">
+                                <li>
+                                    <i class="me-2 fa-solid fa-user"></i>Gerente M.I.S.
+                                </li>
+                                <li>
+                                    <i class="me-2 fa-solid fa-headset"></i>Ext. 1070
+                                </li>
+                                <li>
+                                    <i class="me-2 fa-solid fa-envelope"></i>
+                                    <a href="mailto:karenimartinez@sertec.com.mx"
+                                       class="text-white-50 text-decoration-none">
+                                        <u>Contacto</u>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col mb-3 mb-xl-0">
+                        <div class="card-body">
+                            <h6 class="text-light">Erick M. Pulido Martínez</h6>
+                            <ul class="mb-0 list-unstyled text-white-50">
+                                <li>
+                                    <i class="me-2 fa-solid fa-user"></i>Supervisor M.I.S.
+                                </li>
+                                <li>
+                                    <i class="me-2 fa-solid fa-headset"></i>Ext. 1331
+                                </li>
+                                <li>
+                                    <i class="me-2 fa-solid fa-envelope"></i>
+                                    <a href="mailto:mis.process4@sertec.com.mx"
+                                       class="text-white-50 text-decoration-none">
+                                        <u>Contacto</u>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col mb-3 mb-xl-0">
+                        <div class="card-body">
+                            <h6 class="text-light">J. Alberto Contreras López</h6>
+                            <ul class="mb-0 list-unstyled text-white-50">
+                                <li>
+                                    <i class="me-2 fa-solid fa-user"></i>Especialista Jr. M.I.S.
+                                </li>
+                                <li>
+                                    <i class="me-2 fa-solid fa-headset"></i>Ext. 1337
+                                </li>
+                                <li>
+                                    <i class="me-2 fa-solid fa-envelope"></i>
+                                    <a href="mailto:mis.mexico3@sertec.com.mx"
+                                       class="text-white-50 text-decoration-none">
+                                        <u>Contacto</u>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col mb-3 mb-xl-0">
+                        <div class="card-body">
+                            <h6 class="text-light">Gerardo Carrizosa Valentín</h6>
+                            <ul class="mb-0 list-unstyled text-white-50">
+                                <li>
+                                    <i class="me-2 fa-solid fa-user"></i>Especialista Jr. M.I.S.
+                                </li>
+                                <li>
+                                    <i class="me-2 fa-solid fa-headset"></i>Ext. 1337
+                                </li>
+                                <li>
+                                    <i class="me-2 fa-solid fa-envelope"></i>
+                                    <a href="mailto:mis.mexico6@sertec.com.mx"
+                                       class="text-white-50 text-decoration-none">
+                                        <u>Contacto</u>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mt-2 container-fluid opacity-sertec-30">
+                <p class="mb-0 text-center text-muted">&copy; Servicios Técnicos de Cobranza S.A. de C.V.</p>
+                <p class="mb-0 text-center">
+                    <a target="_blank"
+                       href="https://goo.gl/maps/WDeAR6UpceNQP1r58"
+                       class="text-decoration-none text-muted">
+                        <i class="me-2 fa-solid fa-building"></i>
+                        Av. Benjamín Franklin #98 Col. Escandón C.P. 11800 Ciudad de México
+                    </a>
+                </p>
+            </div>
+        </footer>
---
+++ core/views/errors/_401-Unauthorized.php
@@ -0,0 +1,21 @@
+        <div class="vh-100 d-flex flex-wrap align-content-center bg-dark">
+            <div class="w-100 d-flex justify-content-center container">
+                <div class="p-3 bg-transparent text-white">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-12 col-md-3 d-flex justify-content-center align-items-center">
+                                <i class="fa-regular fa-hand fa-7x"></i>
+                            </div>
+                            <div class="mt-4 mt-md-0 col-12 col-md-9">
+                                <h1 class="display-1 text-center text-md-start font-monospace">401</h1>
+                                <p class="fs-1 text-center text-md-start font-weight-light">Unauthorized</p>
+                                <p class="fs-4">You don't have permission to access the resource.</p>
+                                <p class="mt-4 text-center text-md-start">
+                                    <a href="Dashboard" class="btn btn-outline-light">Back to</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
