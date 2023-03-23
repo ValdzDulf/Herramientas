524ea9a5125c9b1629ef07bba8de74ff8931a163
Diego Valentin <mis.process.cdmx@sertec.com.mx>
2023-03-23 12:43:07 -0600

    feat: Adds method for build options for active managers

--- app/controllers/SelectorBuilder.php
+++ app/controllers/SelectorBuilder.php
@@ -8,6 +8,7 @@ use core\exceptions\ApplicationException;
 use core\models\MessageModel;
 use core\models\WebServiceResponseModel;
 
+use app\models\ManagerDAOModel;
 use app\models\ProfileDAOModel;
 
 /**
@@ -53,6 +54,63 @@ class SelectorBuilder
 
             $webServiceResponse->statusCode = 200;
             $webServiceResponse->message = new MessageModel('success', 'Get Profile List', 'OK');
+            http_response_code(200);
+        } catch (ApplicationException $appEx) {
+            $webServiceResponse->statusCode = 405;
+            $webServiceResponse->message = new MessageModel(
+                    'danger', 'Unauthorized HTTP Method', $appEx->getMessage()
+            );
+            http_response_code(405);
+        } catch (Exception $ex) {
+            $webServiceResponse->message = new MessageModel('danger', 'Uncaught Exception', $ex->getMessage());
+            $webServiceResponse->statusCode = 400;
+            http_response_code(400);
+        } finally {
+            echo json_encode(
+                    $webServiceResponse, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES
+            );
+
+            # Clears explicitly the variables used.
+            foreach (get_defined_vars() as $key => $var) {
+                unset(${$key});
+            }
+        }
+    }
+
+    /**
+     * Builds the options for the active manager selector.
+     *
+     * @return void
+     */
+    public function getActiveManagers()
+    {
+        $webServiceResponse = new WebServiceResponseModel();
+
+        $webServiceResponse->data = [];
+        $webServiceResponse->data[] = ['extra' => null, 'value' => '', 'text' => 'Select an option'];
+
+        try {
+            if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+                throw new ApplicationException('Method Not Allowed');
+            }
+
+            $managerDAO = new ManagerDAOModel();
+            $list = $managerDAO->getActiveManagers();
+
+            foreach ($list as $manager) {
+                $manager['firstSurname'] = Helpers::firstLetterStringUpperCase($manager['firstSurname']);
+                $manager['secondSurname'] = Helpers::firstLetterStringUpperCase($manager['secondSurname']);
+                $manager['firstName'] = Helpers::firstLetterStringUpperCase($manager['firstName']);
+
+                $text = "{$manager['firstSurname']} {$manager['secondSurname']} {$manager['firstName']}";
+                $value = $manager['id'];
+                $extra = null;
+
+                $webServiceResponse->data[] = ['extra' => $extra, 'value' => $value, 'text' => $text];
+            }
+
+            $webServiceResponse->statusCode = 200;
+            $webServiceResponse->message = new MessageModel('success', 'Get Manager List', 'OK');
             http_response_code(200);
         } catch (ApplicationException $appEx) {
             $webServiceResponse->statusCode = 405;
